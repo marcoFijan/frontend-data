7bdd158e364236c516b2ee78073da7449fc5a70a
marcoFijan <marcofijan@hotmail.com>
2020-11-03 17:03:52 +0100

    made chart of RDWData without text yet

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 const proxyURL = 'https://cors-anywhere.herokuapp.com/' // proxylink from Laurens Aarnoudse: Needed for https request for getting the data from local host
-const overviewURL = 'https://npropendata.rdw.nl/parkingdata/v2/'
-const cityLocation = 'groningen'
+const parkingSpecsURL = 'https://opendata.rdw.nl/resource/b3us-f26s.json'
 
 const svg = d3.select('svg')
-//Receiving the data
-const parkingLocationOverview = d3.json(proxyURL + overviewURL)
-  .then(parkingOverview => {
-    const ParkingFacilitiesOverview = parkingOverview.ParkingFacilities
-    const parkingLocation = ParkingFacilitiesOverview.filter(checkLocation)
-    parkingLocation.forEach(location => console.log(location.name, location.limitedAccess))
-    createDiagram(parkingLocation)
-  })
+const width = 700
+const height = 400
 
-//
-const checkLocation = location => {
-  if(!location.name){
-    location.name = 'ONBEKEND'
-  }
-  return location.name.toLowerCase().includes(cityLocation)
-}
+//Receiving the data
+const parkingSpecsOverview = d3.json(proxyURL + parkingSpecsURL)
+  .then(parkingOverview => {
+    // const parkingAreaManagerId = parkingOverview.forEach(parkingGarage => console.log(parkingGarage.areamanagerid))
+    const splittedParkingOverview = parkingOverview.slice(0, 10)
+    console.log(splittedParkingOverview)
+    // const ParkingFacilitiesOverview = parkingOverview.ParkingFacilities
+    // const parkingLocation = ParkingFacilitiesOverview.filter(checkLocation)
+    // parkingLocation.forEach(location => console.log(location.name, location.limitedAccess))
+    createDiagram(splittedParkingOverview)
+  })
 
 //D3 Logic
 const createDiagram = data => {
+  const scaleX = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.chargingpointcapacity)])
+    .range([0, width])
+
+  const scaleY = d3.scaleBand()
+    .domain(data.map(d => d.areamanagerid))
+    .range([0, height])
+
   svg.selectAll('rect').data(data)
     .enter().append('rect')
-      .attr('width', 300)
-      .attr('height', 300)
+      .attr('y', d => scaleY(d.areamanagerid))
+      .attr('width', d => scaleX(d.chargingpointcapacity))
+      .attr('height', scaleY.bandwidth())
 }
