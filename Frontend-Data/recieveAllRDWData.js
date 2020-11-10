2424088ecb0fe86b8258f4c68fa81bd0cf21e156
marcoFijan <marcofijan@hotmail.com>
2020-11-10 15:56:15 +0100

    receiveRDWData.js added to all the data from rdw in a txt file

---
+++ Frontend-Data/recieveAllRDWData.js
@@ -0,0 +1,46 @@
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+const overviewRDWUrl = 'https://npropendata.rdw.nl/parkingdata/v2/'
+
+async function setupData(){
+  // const parkingOverview = await getData(proxyUrl+overviewRDWUrl)
+  const parkingOverview = await getData(overviewRDWUrl)
+  console.log(parkingOverview)
+  const combinedData = await combineData(parkingOverview)
+  console.dir(combinedData)
+  document.getElementById('test').addEventListener('click', download_txt(combinedData))
+}
+
+async function combineData(parkingOverview){
+  const parkingOverviewSliced = parkingOverview.ParkingFacilities
+  const parkingIdentifiers = parkingOverviewSliced.map(garage => garage.identifier)
+  console.log(parkingIdentifiers)
+  // const baseUrl = proxyUrl + overviewRDWUrl + 'static/'
+  const baseUrl = overviewRDWUrl + 'static/'
+
+  parkingFacilityArray = parkingIdentifiers.map(identifier => getData(baseUrl+identifier))
+  console.log(parkingFacilityArray)
+  const dataCollection = await Promise.all(parkingFacilityArray)
+  const dataCollectionArray = dataCollection.map(garage => garage.parkingFacilityInformation)
+  return dataCollectionArray
+}
+
+let counter = 0
+
+async function getData(url){
+  const parkingOverview = await d3.json(url)
+  setTimeout('', 2000)
+  console.log(counter++)
+  return parkingOverview
+}
+
+function download_txt(data) {
+  const textToSave = JSON.stringify(data)
+  const hiddenElement = document.createElement('a')
+
+  hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave)
+  hiddenElement.target = '_blank'
+  hiddenElement.download = 'myFile.txt'
+  hiddenElement.click()
+}
+
+setupData()
